3b111c90d5c509465c0531e1f63ea259fc165609
jiangteng.7 <jiangteng.7@bytedance.com>
2023-06-09 15:13:07 +0800

    增加SimpleStreamer

---
+++ SimpleStreamer/app/src/main/java/com/bytedance/videoarch/ttLiveStream/StartActivity.java
@@ -0,0 +1,46 @@
+//
+//   AppDelegate.m
+//   BDLive
+//
+//   BDLive SDK License
+//
+//   Copyright 2022 Beijing Volcano Engine Technology Ltd. All Rights Reserved.
+//
+//   The BDLive SDK was developed by Beijing Volcanoengine Technology Ltd. (hereinafter “Volcano Engine”).
+//   Any copyright or patent right is owned by and proprietary material of the Volcano Engine.
+//
+//   BDLive SDK is available under the VolcLive product and licensed under the commercial license.
+//   Customers can contact service@volcengine.com for commercial licensing options.
+//   Here is also a link to subscription services agreement: https://www.volcengine.com/docs/6256/68938.
+//
+//   Without Volcanoengine's prior written permission, any use of BDLive SDK, in particular any use for commercial purposes, is prohibited.
+//   This includes, without limitation, incorporation in a commercial product, use in a commercial service, or production of other artefacts for commercial purposes.
+//
+//   Without Volcanoengine's prior written permission, the BDLive SDK may not be reproduced, modified and/or made available in any form to any third party.
+//
+
+package com.bytedance.videoarch.ttLiveStream;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        if (!isTaskRoot()
+                && intent != null
+                && intent.hasCategory(Intent.CATEGORY_LAUNCHER)
+                && intent.getAction() != null
+                && intent.getAction().equals(Intent.ACTION_MAIN)) {
+            finish();
+            return;
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
