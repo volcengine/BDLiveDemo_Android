37764dfb29ca3c07cd2d5189f15b8eb1eef31f09
jiangteng.7 <jiangteng.7@bytedance.com>
2024-05-24 15:18:43 +0800

    feat：SimpleStreamer增加CV接入部分

--- SimpleStreamer/app/src/main/java/com/bytedance/videoarch/ttLiveStream/MainActivity.java
+++ SimpleStreamer/app/src/main/java/com/bytedance/videoarch/ttLiveStream/MainActivity.java
@@ -29,6 +29,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bytedance.live.common.utils.ToastUtil;
 import com.bytedance.live.push.TVUPushLiveRoom;
+import com.bytedance.live.push.logic.settings.CVSettings;
+import com.bytedance.live.push.logic.settings.PushSettings;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,12 +45,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         idEditText = findViewById(R.id.edittext_activityid);
         tokenEditText = findViewById(R.id.edittext_token);
 
         idEditText.setText(activityId);
         tokenEditText.setText(secretKey);
+        openCV();
     }
 
 
@@ -63,6 +65,22 @@ public class MainActivity extends AppCompatActivity {
         String token = tokenEditText.getText().toString().trim();
 
         TVUPushLiveRoom.startPushLiveRoom(this, id, token);
+    }
+
+    private void openCV(){
+        CVSettings cvSettings = PushSettings.Holder.mSettings.getCvSettings();
+        //开启CV
+        cvSettings.setEnableCV(true);
+
+        //设置CV资源包对应版本号，版本号变大后SDK会重新解压CV资源包，建议每次更新CV资源包后版本号+1
+        cvSettings.setResourceVersionCode(1);
+
+        //设置CV资源包在Android assets目录中的路径
+        cvSettings.setResourceAssetName("resource.zip");
+
+        //设置CV在线授权的LicenseKey和LicenseSecret
+        cvSettings.setOnLineLicenseInfo("biz_license_tool_test_keyec8cd09ce1cd4aa7858ddd23cf1822cb",
+                "aacf70f12f4c0b53cdc4c25d8ef3c81a");
     }
 
 }
